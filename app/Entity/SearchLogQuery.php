2ee20265c6d4d19d8dc046f495da71758987de31
Mikhail Solovev <mixailo@mixailo.org>
2022-04-03 16:47:18 +0300

    добавлена сущность SearchLogQuery

--- app/Entity/SearchLogQuery.php
+++ app/Entity/SearchLogQuery.php
@@ -12,14 +12,20 @@ class SearchLogQuery extends Query
     protected bool $isNatural = false;
 
     /**
+     * @var string|null
+     */
+    protected ?string $domain = null;
+
+    /**
      * @param string $query
      * @param LanguageInterface $language
      * @param bool $isNatural
      */
-    public function __construct(string $query, LanguageInterface $language, bool $isNatural = false)
+    public function __construct(string $query, LanguageInterface $language, bool $isNatural = false, ?string $domain = null)
     {
         parent::__construct($query, $language);
         $this->isNatural = $isNatural;
+        $this->domain = $domain;
     }
 
     /**
@@ -28,5 +34,13 @@ class SearchLogQuery extends Query
     public function isNatural(): bool
     {
         return $this->isNatural;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getDomain(): ?string
+    {
+        return $this->domain;
     }
 }
